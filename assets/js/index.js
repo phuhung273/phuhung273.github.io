5c74e8c96ecda63d2e407faac4130bc5a7cca623
phuhung273 <phuhung.tranpham@gmail.com>
2019-11-08 16:57:01 +0700

    first push

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 50,
+  nav: true,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 2
+    },
+    1000: {
+      items: 3
+    }
+  }
+})
