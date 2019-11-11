de4ae45497c46e06f5e454e69ec5423c077272fd
phuhung273 <phuhung.tranpham@gmail.com>
2019-11-11 23:03:56 +0700

    4th commit form validate, modify rtp

--- assets/js/index.js
+++ assets/js/index.js
@@ -18,42 +18,6 @@ $('.owl-carousel').owlCarousel({
   }
 });
 
-$(".form-info").validate({
-  onfocusout: false,
-  onkeyup: false,
-  onclick: false,
-  rules: {
-    "user": {
-      required: true
-    },
-    "phone": {
-      required: true,
-      digits: true
-    },
-    "money": {
-      required: true
-    },
-    "income": {
-      required: true
-    }
-  },
-  messages: {
-    "user": {
-      required: "Vui lòng nhập họ tên"
-    },
-    "phone": {
-      required: "Vui lòng nhập số điện thoại",
-      digits: "Số điện thoại chỉ bao gồm số"
-    },
-    "money": {
-      required: "Vui lòng nhập số tiền cần vay"
-    },
-    "income": {
-      required: "Vui lòng chọn mức thu nhập"
-    }
-  }
-});
-
 $("#page-form").validate({
   onfocusout: false,
   onkeyup: false,
